bb578381fb739674abb704cf55f4f60e3c93fed5
yanghb <yanghb@msan.cn>
2014-10-10 17:31:50 +0800

    fix bug

--- src/android/com/mobishift/plugin/AVOSAndroidPushPlugin.java
+++ src/android/com/mobishift/plugin/AVOSAndroidPushPlugin.java
@@ -1,6 +1,7 @@
 package com.mobishift.plugin;
 
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
 import com.avos.avoscloud.AVInstallation;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -10,17 +11,20 @@ import org.json.JSONObject;
  * @author deckmon
  */
 
-public class PushInstallationPlugin extends CordovaPlugin {
-    public static final String TAG = "PushInstallationPlugin";
+public class AVOSAndroidPushPlugin extends CordovaPlugin {
+    public static final String TAG = "AVOSAndroidPushPlugin";
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) {
+        boolean result = false;
+        
         if (action.equals("get_installation_id")) {
+            result = true;
             callbackContext.success(AVInstallation.getCurrentInstallation().getInstallationId());
         } else {
-            return false;
+            result = false;
         }
 
-        return true;
+        return result;
     }
 }
