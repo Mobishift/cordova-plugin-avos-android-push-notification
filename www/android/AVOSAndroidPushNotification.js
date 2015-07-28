ba923019d4dc41ce5f8a5532f5f7294a6fe1c9be
Zhengz <zhengz@masn.cn>
2015-07-28 17:50:42 +0800

    add ios js

---
+++ www/android/AVOSAndroidPushNotification.js
@@ -0,0 +1,56 @@
+var exec = require('cordova/exec'),
+    channel = require('cordova/channel'),
+    cordova = require('cordova');
+
+var AVOSAndroidPushNotification = function() {
+};
+
+
+// Call this to register for push notifications. Content of [options] depends on whether we are working with APNS (iOS) or GCM (Android)
+AVOSAndroidPushNotification.prototype.get_installation_id = function(successCallback, errorCallback) {
+    if (errorCallback == null) { errorCallback = function() {}}
+
+    if (typeof errorCallback != "function")  {
+        console.log("AVOSAndroidPushNotification.get_installation_id failure: failure parameter not a function");
+        return
+    }
+
+    if (typeof successCallback != "function") {
+        console.log("AVOSAndroidPushNotification.get_installation_id failure: success callback parameter must be a function");
+        return
+    }
+
+    exec(successCallback, errorCallback, "AVOSAndroidPushPlugin", "get_installation_id", []);
+};
+
+AVOSAndroidPushNotification.prototype.on_notification = function(callback){
+    callback = callback || function(){};
+    if(typeof callback !== 'function'){
+        console.log('AVOSAndroidPushNotification.on_notification failure: callback parameter must be a function');
+    }
+    
+    exec(callback, function(){}, "AVOSAndroidPushPlugin", 'on_notification', []);
+};
+
+AVOSAndroidPushNotification.prototype.getUrl = function(callback){
+    callback = callback || function(){};
+    if(typeof callback !== 'function'){
+        console.log('AVOSAndroidPushNotification.getUrl failure: callback parameter must be a function');
+    }
+
+    exec(callback, function(){}, "AVOSAndroidPushPlugin", "get_url", []);
+};
+
+var pushNotification = new AVOSAndroidPushNotification();
+
+channel.onCordovaReady.subscribe(function(){
+    pushNotification.getUrl(function(data){
+        if(data){
+            cordova.fireDocumentEvent('appOpenWithURL', data);
+        }
+    });
+});
+
+//-------------------------------------------------------------------
+
+module.exports = pushNotification;
--- www/ios/AVOSAndroidPushNotification.js
+++ www/ios/AVOSAndroidPushNotification.js
@@ -8,34 +8,17 @@ var AVOSAndroidPushNotification = function() {
 
 // Call this to register for push notifications. Content of [options] depends on whether we are working with APNS (iOS) or GCM (Android)
 AVOSAndroidPushNotification.prototype.get_installation_id = function(successCallback, errorCallback) {
-    if (errorCallback == null) { errorCallback = function() {}}
-
-    if (typeof errorCallback != "function")  {
-        console.log("AVOSAndroidPushNotification.get_installation_id failure: failure parameter not a function");
-        return
-    }
-
-    if (typeof successCallback != "function") {
-        console.log("AVOSAndroidPushNotification.get_installation_id failure: success callback parameter must be a function");
-        return
-    }
-
-    exec(successCallback, errorCallback, "AVOSAndroidPushPlugin", "get_installation_id", []);
+    console.log('this methpd has been declined in ios');
 };
 
 AVOSAndroidPushNotification.prototype.on_notification = function(callback){
-    callback = callback || function(){};
-    if(typeof callback !== 'function'){
-        console.log('AVOSAndroidPushNotification.on_notification failure: callback parameter must be a function');
-    }
-    
-    exec(callback, function(){}, "AVOSAndroidPushPlugin", 'on_notification', []);
+    console.log('this methpd has been declined in ios');
 };
 
 AVOSAndroidPushNotification.prototype.getUrl = function(callback){
     callback = callback || function(){};
     if(typeof callback !== 'function'){
-        console.log('AVOSAndroidPushNotification.on_notification failure: callback parameter must be a function');
+        console.log('AVOSAndroidPushNotification.getUrl failure: callback parameter must be a function');
     }
 
     exec(callback, function(){}, "AVOSAndroidPushPlugin", "get_url", []);
